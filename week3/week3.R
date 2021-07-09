5c10ebaee77ab25785c54059c277dc7225778ce4
Wenjie Zhao <wjzhao@ucsc.edu>
2021-07-09 15:05:20 -0700

    fix the bugs for computing the variance

--- week3/week3.R
+++ week3/week3.R
@@ -12,18 +12,18 @@ library(dlm)
 ##### First order polynomial #####
 ### forward update equations ###
 forward_filter <- function(data, model){
-  
+
   ## retrieve dataset
   yt <- data$yt
   num_timesteps <- length(yt)
-  
-  ## retrieve a set of quadruples 
+
+  ## retrieve a set of quadruples
   # FF, GG, VV, WW are scalar
-  FF <- model$FF  
+  FF <- model$FF
   GG <- model$GG
   VV <- model$V
   WW <- model$W
-  
+
   ## retrieve initial states
   m0 <- model$m0
   C0 <- model$C0
@@ -37,8 +37,8 @@ forward_filter <- function(data, model){
   mt <- matrix(NA, nrow=num_timesteps, ncol=d)
   Ct <- array(NA, dim=c(d, d, num_timesteps))
   et <- numeric(num_timesteps)
-  
-  
+
+
   for(i in 1:num_timesteps){
     # moments of priors at t
     if(i == 1){
@@ -48,11 +48,11 @@ forward_filter <- function(data, model){
       at[i, ] <- GG %*% t(mt[i-1, , drop=FALSE])
       Rt[, , i] <- GG %*% Ct[, , i-1] %*% t(GG) + WW
     }
-    
+
     # moments of one-step forecast:
-    ft[i] <- t(FF) %*% t(at[i, ]) 
+    ft[i] <- t(FF) %*% t(at[i, ])
     Qt[i] <- t(FF) %*% Rt[, , i] %*% FF + VV
-    
+
     # moments of posterior at t:
     At <- Rt[, , i] %*% FF / Qt[i]
     et[i] <- yt[i] - ft[i]
@@ -65,21 +65,21 @@ forward_filter <- function(data, model){
 
 ### smoothing function ###
 backward_smoothing <- function(data, model, posterior_states){
-  ## retrieve data 
+  ## retrieve data
   yt <- data$yt
-  num_timesteps <- length(yt) 
-    
+  num_timesteps <- length(yt)
+
   ## retrieve parameters
   FF <- model$FF
   GG <- model$GG
-  
+
   ## retrieve parameters
   mt <- posterior_states$mt
   Ct <- posterior_states$Ct
   at <- posterior_states$at
   Rt <- posterior_states$Rt
-  
-  ## create placeholder for posterior moments 
+
+  ## create placeholder for posterior moments
   mnt <- matrix(NA, nrow = dim(mt)[1], ncol = dim(mt)[2])
   Cnt <- array(NA, dim = dim(Ct))
   fnt <- numeric(num_timesteps)
@@ -112,18 +112,18 @@ forecast_function <- function(posterior_states, k, model){
   VV <- model$V
   mt <- posterior_states$mt
   Ct <- posterior_states$Ct
-  
+
   ## set up parameters
   num_timesteps <- dim(mt)[1] # time points
   d <- dim(mt)[2]             # dimension of state parameters
-  
+
   ## placeholder for results
   at <- matrix(NA, nrow = k, ncol = d)
   Rt <- array(NA, dim=c(d, d, k))
   ft <- numeric(k)
   Qt <- numeric(k)
-  
-  
+
+
   for(i in 1:k){
     ## moments of state distribution
     if(i == 1){
@@ -133,7 +133,7 @@ forecast_function <- function(posterior_states, k, model){
       at[i, ] <- GG %*% t(at[i-1, , drop=FALSE])
       Rt[, , i] <- GG %*% Rt[, , i-1] %*% t(GG) + WW
     }
-    
+
     ## moments of forecast distribution
     ft[i] <- t(FF) %*% t(at[i, , drop=FALSE])
     Qt[i] <- t(FF) %*% Rt[, , i] %*% FF + VV
@@ -154,11 +154,11 @@ get_credible_interval <- function(ft, Qt, quantile = c(0.025, 0.975)){
 
 myplot <- function(results_filtered, results_smoothed, results_forecast, ci=TRUE){
   par(mfrow=c(1,1), cex.main=1.5, cex.lab=1.5)
-  
+
   index <- seq(1871, 1970, length.out = length(Nile))
   index_train <- index[1:length(data$yt)]
   index_valid <- index[(length(data$yt)+1):length(Nile)]
-  
+
   plot(index, Nile, pch=1, ylab = "level", main = "Nile River Level")
   lines(index_train, results_filtered$mt, type='l', col='red')
   lines(index_valid, results_forecast$ft, type='l', col='green')
@@ -202,8 +202,8 @@ ci_smoothed <- get_credible_interval(results_smoothed$mnt, results_smoothed$Cnt)
 
 
 ## plot results
-myplot(results_filtered = results_filtered, 
-       results_smoothed = results_smoothed, 
+myplot(results_filtered = results_filtered,
+       results_smoothed = results_smoothed,
        results_forecast = results_forecast, ci=TRUE)
 
 
@@ -213,25 +213,25 @@ myplot(results_filtered = results_filtered,
 ##### more generic version
 ### forward update equations ###
 forward_filter <- function(data, parameters, initial_states, delta){
-  
+
   ## retrieve datasets
   yt <- data$yt
   num_timesteps<- length(yt)
-  
+
   ## retrieve parameters
   Ft <- parameters$Ft
   Gt <- parameters$Gt
   if(missing(delta)){
     Wt_star <- parameters$Wt_star
   }
-  
+
   ## retrieve initial state
   m0 <- initial_states$m0
   C0_star <- initial_states$C0_star
   n0 <- initial_states$n0
   S0 <- initial_states$S0
   C0 <- S0*C0_star
-  
+
   ## create placeholder for results
   d <- dim(Gt)[1]
   at <- matrix(NA, nrow=num_timesteps, ncol=d)
@@ -244,7 +244,7 @@ forward_filter <- function(data, parameters, initial_states, delta){
   nt <- numeric(num_timesteps)
   St <- numeric(num_timesteps)
   dt <- numeric(num_timesteps)
-  
+
   # moments of priors at t
   for(i in 1:num_timesteps){
     if(i == 1){
@@ -256,7 +256,7 @@ forward_filter <- function(data, parameters, initial_states, delta){
       }else{
         Rt[, , i] <- Pt/delta
       }
-      
+
     }else{
       at[i, ] <- Gt[, , i] %*% t(mt[i-1, , drop=FALSE])
       Pt <- Gt[, , i] %*% Ct[, , i-1] %*% t(Gt[, , i])
@@ -267,15 +267,15 @@ forward_filter <- function(data, parameters, initial_states, delta){
         Rt[, , i] <- Pt/delta
       }
     }
-    
+
     # moments of one-step forecast:
-    ft[i] <- t(Ft[, , i]) %*% t(at[i, , drop=FALSE]) 
+    ft[i] <- t(Ft[, , i]) %*% t(at[i, , drop=FALSE])
     Qt[i] <- t(Ft[, , i]) %*% Rt[, , i] %*% Ft[, , i] + ifelse(i==1, S0, St[i-1])
     et[i] <- yt[i] - ft[i]
-    
+
     nt[i] <- ifelse(i==1, n0, nt[i-1]) + 1
     St[i] <- ifelse(i==1, S0, St[i-1])*(1 + 1/nt[i]*(et[i]^2/Qt[i]-1))
-   
+
     # moments of posterior at t:
     At <- Rt[, , i] %*% Ft[, , i] / Qt[i]
 
@@ -283,19 +283,19 @@ forward_filter <- function(data, parameters, initial_states, delta){
     Ct[, , i] <- St[i]/ifelse(i==1, S0, St[i-1])*(Rt[, , i] - Qt[i] * At %*% t(At))
   }
   cat("Forward filtering is completed!\n")
-  return(list(mt = mt, Ct = Ct, 
-              at = at, Rt = Rt, 
-              ft = ft, Qt = Qt, 
-              et = et, 
+  return(list(mt = mt, Ct = Ct,
+              at = at, Rt = Rt,
+              ft = ft, Qt = Qt,
+              et = et,
               nt = nt, St = St))
 }
 
 ### smoothing function ###
 backward_smoothing <- function(data, parameters, posterior_states, delta){
-  ## retrieve data 
+  ## retrieve data
   yt <- data$yt
-  num_timesteps <- length(yt) 
-  
+  num_timesteps <- length(yt)
+
   ## retrieve parameters
   if(missing(delta)){
     Ft <- parameters$Ft
@@ -309,13 +309,13 @@ backward_smoothing <- function(data, parameters, posterior_states, delta){
   nt <- posterior_states$nt
   St <- posterior_states$St
   at <- posterior_states$at
-  
-  ## create placeholder for posterior moments 
+
+  ## create placeholder for posterior moments
   mnt <- matrix(NA, nrow = dim(mt)[1], ncol = dim(mt)[2])
   Cnt <- array(NA, dim = dim(Ct))
   fnt <- numeric(num_timesteps)
   Qnt <- numeric(num_timesteps)
-  
+
   for(i in num_timesteps:1){
     if(i == num_timesteps){
       mnt[i, ] <- mt[i, ]
@@ -329,11 +329,16 @@ backward_smoothing <- function(data, parameters, posterior_states, delta){
       }else{
         inv_Gt <- solve(Gt[, , i+1])
         mnt[i, ] <- (1-delta)*mt[i, ] + delta*inv_Gt %*% t(mnt[i+1, , drop=FALSE])
-        Cnt[, , i] <- St[num_timesteps]/St[i]*((1-delta)*Ct[, , i] + delta^2*inv_Gt %*% Cnt[, , i + 1]  %*% t(inv_Gt))
+        Cnt[, , i] <- ((1-delta)*Ct[, , i] + delta^2*inv_Gt %*% Cnt[, , i + 1]  %*% t(inv_Gt))
       }
     }
+
     fnt[i] <- t(Ft[, , i]) %*% t(mnt[i, , drop=FALSE])
     Qnt[i] <- t(Ft[, , i]) %*% t(Cnt[, , i]) %*% Ft[, , i]
+  }
+  for(i in 1:num_timesteps){
+    Cnt[, , i] <-  St[num_timesteps]/St[i]*Cnt[, , i]
+    Qnt[i] <- St[num_timesteps]/St[i]*Qnt[i]
   }
   cat("Backward smoothing is completed!\n")
   return(list(mnt = mnt, Cnt = Cnt, fnt=fnt, Qnt=Qnt))
@@ -341,40 +346,40 @@ backward_smoothing <- function(data, parameters, posterior_states, delta){
 
 ## Forecast Distribution for k step
 forecast_function <- function(posterior_states, k, parameters, delta){
-  
+
   ## retrieve parameters
   Ft <- parameters$Ft
   Gt <- parameters$Gt
   if(missing(delta)){
     Wt_star <- parameters$Wt_star
   }
-  
+
   mt <- posterior_states$mt
   Ct <- posterior_states$Ct
   St <- posterior_states$St
   at <- posterior_states$at
-  
+
   ## set up parameters
   num_timesteps <- dim(mt)[1] # time points
   d <- dim(mt)[2]             # dimension of state parameters
-  
+
   ## placeholder for results
   at <- matrix(NA, nrow = k, ncol = d)
   Rt <- array(NA, dim=c(d, d, k))
   ft <- numeric(k)
   Qt <- numeric(k)
-  
+
   for(i in 1:k){
     ## moments of state distribution
     if(i == 1){
       at[i, ] <- Gt[, , num_timesteps+i] %*% t(mt[num_timesteps, , drop=FALSE])
-      
+
       if(missing(delta)){
         Rt[, , i] <- Gt[, , num_timesteps+i] %*% Ct[, , num_timesteps] %*% t(Gt[, , num_timesteps+i]) + St[num_timesteps]*Wt_star[, , num_timesteps+i]
       }else{
         Rt[, , i] <- Gt[, , num_timesteps+i] %*% Ct[, , num_timesteps] %*% t(Gt[, , num_timesteps+i])/delta
       }
-      
+
     }else{
       at[i, ] <- Gt[, , num_timesteps+i] %*% t(at[i-1, , drop=FALSE])
       if(missing(delta)){
@@ -383,7 +388,7 @@ forecast_function <- function(posterior_states, k, parameters, delta){
         Rt[, , i] <- Gt[, , num_timesteps+i] %*% Rt[, , i-1] %*% t(Gt[, , num_timesteps+i])/delta
       }
     }
-    
+
     ## moments of forecast distribution
     ft[i] <- t(Ft[, , num_timesteps+i]) %*% t(at[i, , drop=FALSE])
     Qt[i] <- t(Ft[, , num_timesteps+i]) %*% Rt[, , i] %*% Ft[, , num_timesteps+i] + St[num_timesteps]
@@ -415,11 +420,11 @@ get_credible_interval <- function(ft, Qt, nt, quantile = c(0.025, 0.975)){
   bound <- matrix(0, nrow=length(ft), ncol=2)
   # lower bound of 95% credible interval
   z_quantile <- qt(quantile[1], df = nt)
-  bound[, 1] <- ft + z_quantile*sqrt(as.numeric(Qt)) 
+  bound[, 1] <- ft + z_quantile*sqrt(as.numeric(Qt))
 
   # upper bound of 95% credible interval
   z_quantile <- qt(quantile[2], df = nt)
-  bound[, 2] <- ft + z_quantile*sqrt(as.numeric(Qt)) 
+  bound[, 2] <- ft + z_quantile*sqrt(as.numeric(Qt))
   return(bound)
 }
 
@@ -450,12 +455,12 @@ ci_filtered <- get_credible_interval(results_filtered$mt, results_filtered$Ct, r
 
 ## smoothing
 results_smoothed <- backward_smoothing(data, parameters, results_filtered)
-ci_smoothed <- get_credible_interval(results_smoothed$mnt, results_smoothed$Cnt, 
+ci_smoothed <- get_credible_interval(results_smoothed$mnt, results_smoothed$Cnt,
                                      results_filtered$nt[num_timesteps])
 
 ## one-step ahead forecasting
 results_forecast <- forecast_function(results_filtered, length(valid_data), parameters)
-ci_forecast <- get_credible_interval(results_forecast$ft, results_forecast$Qt, 
+ci_forecast <- get_credible_interval(results_forecast$ft, results_forecast$Qt,
                                      results_filtered$nt[num_timesteps])
 
 
@@ -513,10 +518,10 @@ adaptive_dlm <- function(data, parameters, initial_states, df_range, type, forec
     results_tmp <- forward_filter(data, parameters, initial_states, i)
 
     measure[j] <- measure_forecast_accuracy(et=results_tmp$et, yt=data$yt,
-                                            Qt=results_tmp$Qt, nt=c(initial_states$n0, results_tmp$nt), 
+                                            Qt=results_tmp$Qt, nt=c(initial_states$n0, results_tmp$nt),
                                             type=type)
 
-    
+
     if(j == 1){
       measure_best <- measure[j]
       results_filtered <- results_tmp
@@ -530,16 +535,16 @@ adaptive_dlm <- function(data, parameters, initial_states, df_range, type, forec
   results_smoothed <- backward_smoothing(data, parameters, results_filtered, delta = df_opt)
   if(forecast){
     results_forecast <- forecast_function(results_filtered, length(valid_data), parameters, df_opt)
-    return(list(results_filtered=results_filtered, 
-                results_smoothed=results_smoothed, 
-                results_forecast=results_forecast, 
+    return(list(results_filtered=results_filtered,
+                results_smoothed=results_smoothed,
+                results_forecast=results_forecast,
                 df_opt = df_opt, measure=measure))
   }else{
-    return(list(results_filtered=results_filtered, 
-                results_smoothed=results_smoothed, 
+    return(list(results_filtered=results_filtered,
+                results_smoothed=results_smoothed,
                 df_opt = df_opt, measure=measure))
   }
-  
+
 }
 
 
@@ -579,7 +584,7 @@ ci_filtered <- get_credible_interval(results_filtered$mt, results_filtered$Ct, r
 
 ## retrieve smoothed results
 results_smoothed <- results_MSE$results_smoothed
-ci_smoothed <- get_credible_interval(results_smoothed$mnt, results_smoothed$Cnt, 
+ci_smoothed <- get_credible_interval(results_smoothed$mnt, results_smoothed$Cnt,
                                      results_filtered$nt[num_timesteps])
 
 ## retrieve one-step-ahead forecast results
@@ -602,13 +607,13 @@ print(paste("The selected discount factor:", results_MAD$df_opt))
 
 ## retrieve filtered results
 results_filtered <- results_MAD$results_filtered
-ci_filtered <- get_credible_interval(results_filtered$mt, 
-                                     results_filtered$Ct, 
+ci_filtered <- get_credible_interval(results_filtered$mt,
+                                     results_filtered$Ct,
                                      results_filtered$nt)
 
 ## retrieve smoothed results
 results_smoothed <- results_MAD$results_smoothed
-ci_smoothed <- get_credible_interval(results_smoothed$mnt, results_smoothed$Cnt, 
+ci_smoothed <- get_credible_interval(results_smoothed$mnt, results_smoothed$Cnt,
                                      results_filtered$nt[num_timesteps])
 
 ## retrieve one-step-ahead forecast results
@@ -666,7 +671,7 @@ df_range <- seq(0.8, 1, by = .001)
 
 ## fit discount DLM
 ## MSE
-results_MSE <- adaptive_dlm(data, parameters, initial_states, 
+results_MSE <- adaptive_dlm(data, parameters, initial_states,
                             df_range, "MSE", forecast = FALSE)
 
 ## print selected discount factor
@@ -674,12 +679,12 @@ print(paste("The selected discount factor:", results_MSE$df_opt))
 
 ## retrieve filtered results
 results_filtered <- results_MSE$results_filtered
-ci_filtered <- get_credible_interval(results_filtered$ft, results_filtered$Qt, 
+ci_filtered <- get_credible_interval(results_filtered$ft, results_filtered$Qt,
                                      results_filtered$nt)
 
 ## retrieve smoothed results
 results_smoothed <- results_MSE$results_smoothed
-ci_smoothed <- get_credible_interval(results_smoothed$fnt, results_smoothed$Qnt, 
+ci_smoothed <- get_credible_interval(results_smoothed$fnt, results_smoothed$Qnt,
                                      results_filtered$nt[length(results_smoothed$fnt)])
 
 ## plot results
